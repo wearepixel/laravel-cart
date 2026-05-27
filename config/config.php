3b8fa4c83820a69485cef7a6752cfacbd08aee16
Joel Male <joel@joelmale.com>
2026-05-27 21:02:32 +1000

    feat: update cart config with v3 driver options

--- config/config.php
+++ config/config.php
@@ -16,16 +16,30 @@ return [
      * ---------------------------------------------------------------
      * Storage
      * ---------------------------------------------------------------
+     * Supported: "session", "database", "redis", "null", "multi"
      */
     'driver' => 'session',
 
     'storage' => [
-        'session',
         'database' => [
-            'model' => '',
-            'id' => '',
-            'items' => '',
+            'model'      => '',
+            'id'         => '',
+            'items'      => '',
             'conditions' => '',
         ],
+    ],
+
+    /*
+     * ---------------------------------------------------------------
+     * Driver-specific options
+     * ---------------------------------------------------------------
+     */
+    'drivers' => [
+        'redis' => [
+            'connection' => 'default',
+            'ttl'        => 60 * 24 * 7, // 7 days in minutes
+        ],
+
+        'multi' => ['session', 'database'],
     ],
 ];
